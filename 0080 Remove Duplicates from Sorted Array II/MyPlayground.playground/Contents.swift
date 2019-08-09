c673e14fc6291bb78e0607f30dc7adaa2d2cf5df
Haik <haik.ampardjian@gmail.com>
2019-08-10 01:09:35 +0300

    Add 26 and 80

---
+++ 0080 Remove Duplicates from Sorted Array II/MyPlayground.playground/Contents.swift
@@ -0,0 +1,69 @@
+/**
+ Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+ 
+ Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ 
+ Example 1:
+ 
+ Given nums = [1,1,1,2,2,3],
+ 
+ Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+ 
+ It doesn't matter what you leave beyond the returned length.
+ Example 2:
+ 
+ Given nums = [0,0,1,1,1,1,2,3,3],
+ 
+ Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+ 
+ It doesn't matter what values are set beyond the returned length.
+ Clarification:
+ 
+ Confused why the returned value is an integer but your answer is an array?
+ 
+ Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+ 
+ Internally you can think of this:
+ 
+ // nums is passed in by reference. (i.e., without making a copy)
+ int len = removeDuplicates(nums);
+ 
+ // any modification to nums in your function would be known by the caller.
+ // using the length returned by your function, it prints the first len elements.
+ for (int i = 0; i < len; i++) {
+ print(nums[i]);
+ }
+ */
+
+import UIKit
+
+class Solution {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        var counter = 0
+        var index = 0
+        var previous = (value: 0, appearanceCounter: 0)
+        
+        while index < nums.count {
+            if nums[index] == previous.value && previous.appearanceCounter >= 2 {
+                nums.remove(at: index)
+                previous.appearanceCounter += 1
+            } else if nums[index] == previous.value && previous.appearanceCounter < 2 {
+                previous.appearanceCounter += 1
+                index += 1
+                counter += 1
+            } else {
+                previous.value = nums[index]
+                previous.appearanceCounter = 1
+                counter += 1
+                index += 1
+            }
+        }
+        
+        print(nums)
+        
+        return counter
+    }
+}
+
+var nums = [0,0,1,1,1,1,2,3,3]
+print(Solution().removeDuplicates(&nums))
